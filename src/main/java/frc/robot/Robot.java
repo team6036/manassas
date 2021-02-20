90cc48f7e56fc48db83dc4495a9359697c5cfceb
frc6036 <frc6036@gmail.com>
2021-02-19 17:26:08 -0800

    fixed module angle issues

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,30 +15,28 @@ public class Robot extends TimedRobot {
 
     @Override
     public void robotInit() {
-        double offsetX = Util.inchesToMeters(26/2);
-        double offsetY = Util.inchesToMeters(24/2);
+        double offsetX = Util.inchesToMeters(26 / 2);
+        double offsetY = Util.inchesToMeters(24 / 2);
         swerve = new SwerveController(
-            new Module(1, 2, 9, new Pose2D(+offsetX, +offsetY, Util.normalizeAngle(-4.473, Math.PI))),
-            new Module(3, 4, 10, new Pose2D(-offsetX, +offsetY, Util.normalizeAngle(-3.646, Math.PI))),
-            new Module(5, 6, 11, new Pose2D(-offsetX, -offsetY, Util.normalizeAngle(-3.764, Math.PI))),
-            new Module(7, 8, 12, new Pose2D(+offsetX, -offsetY, Util.normalizeAngle(-0.005, Math.PI)))
-        );
+                new Module(1, 2, 9, new Pose2D(+offsetX, +offsetY, Util.normalizeAngle(-4.473, Math.PI)), "backRight"),
+                new Module(3, 4, 10, new Pose2D(-offsetX, +offsetY, Util.normalizeAngle(-3.646, Math.PI)),
+                        "frontRight"),
+                new Module(5, 6, 11, new Pose2D(-offsetX, -offsetY, Util.normalizeAngle(-3.764, Math.PI)), "frontLeft"),
+                new Module(7, 8, 12, new Pose2D(+offsetX, -offsetY, Util.normalizeAngle(-0.005, Math.PI)), "backLeft"));
         stick = new Joystick(0);
     }
 
     @Override
     public void teleopPeriodic() {
-        Pose2D robotSpeeds = new Pose2D(-stick.getY(), -stick.getX(), 0);
+        Pose2D robotSpeeds = new Pose2D(-stick.getY(), -stick.getX(), 0).scalarMult(10 * stick.getThrottle());
         swerve.nyoom(robotSpeeds, true, true);
-        log("0", swerve.modules[0].currentAngle);
-        log("1", swerve.modules[1].currentAngle);
-        log("2", swerve.modules[2].currentAngle);
-        log("3", swerve.modules[3].currentAngle);
+        for (int i = 0; i < 4; i++) {
+            log(swerve.modules[i].name + "velocity", swerve.modules[i].currentDriveSpeed);
+        }
     }
 
-    public void log(String key, double val){
+    public void log(String key, double val) {
         SmartDashboard.putNumber(key, val);
     }
-
 
 }
--- src/main/java/frc/robot/common/SwerveController.java
+++ src/main/java/frc/robot/common/SwerveController.java
@@ -4,6 +4,9 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.sensors.CANCoder;
+import com.ctre.phoenix.sensors.SensorInitializationStrategy;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class SwerveController {
 
@@ -12,18 +15,19 @@ public class SwerveController {
     static final double DRIVE_RATIO = 6.86;
     static final double WHEEL_RADIUS = Util.inchesToMeters(2);
 
-    public SwerveController(Module... modules){
+    public SwerveController(Module... modules) {
         this.modules = modules;
     }
 
-    public void nyoom(Pose2D robotSpeeds, boolean turn, boolean drive){
-        for(Module module : modules){
+    public void nyoom(Pose2D robotSpeeds, boolean turn, boolean drive) {
+        for (Module module : modules) {
             module.move(robotSpeeds, turn, drive);
         }
     }
 
-    public static class Module{
+    public static class Module {
 
+        public String name;
         public WPI_TalonFX turnMotor, driveMotor;
         public CANCoder cancoder;
 
@@ -35,18 +39,20 @@ public class SwerveController {
 
         public double currentAngle, currentDriveSpeed;
 
-        //stuff for odometry:
+        // stuff for odometry:
         double lastAngle, lastDrivePos, currentDrivePos;
 
-        public Module(int turnMotorID, int driveMotorID, int cancoderID, Pose2D pose2d){
-
+        public Module(int turnMotorID, int driveMotorID, int cancoderID, Pose2D pose2d, String name) {
+            this.name = name;
             turnMotor = new WPI_TalonFX(turnMotorID);
             driveMotor = new WPI_TalonFX(driveMotorID);
             cancoder = new CANCoder(cancoderID);
             this.placement = pose2d;
 
+            driveMotor.configFactoryDefault();
             turnMotor.configFactoryDefault();
             cancoder.configFactoryDefault();
+            cancoder.configSensorInitializationStrategy(SensorInitializationStrategy.BootToAbsolutePosition);
 
             turnMotor.configRemoteFeedbackFilter(cancoder, 0);
             turnMotor.configSelectedFeedbackSensor(TalonFXFeedbackDevice.RemoteSensor0, 0, 0);
@@ -58,17 +64,17 @@ public class SwerveController {
 
             driveMotor.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 0);
 
-            driveMotor.config_kF(0, 1023.0/20660.0, 0);
+            driveMotor.config_kF(0, 1023.0 / 20660.0, 0);
             driveMotor.config_kP(0, 0.1, 0);
             driveMotor.config_kI(0, 0, 0);
             driveMotor.config_kD(0, 0, 0);
         }
 
-        public void move(Pose2D robotSpeeds, boolean turn, boolean drive){
+        public void move(Pose2D robotSpeeds, boolean turn, boolean drive) {
             Vector2D linVelo = robotSpeeds.getVector2D();
             double angVelo = robotSpeeds.ang;
 
-            if(linVelo.getMagnitude() < 0.1 && Math.abs(angVelo) < 0.1){
+            if (linVelo.getMagnitude() < 0.1 && Math.abs(angVelo) < 0.1) {
                 linVelo = new Vector2D();
                 angVelo = 0;
             }
@@ -82,19 +88,18 @@ public class SwerveController {
             currentDrivePos = getDrivePos();
             currentDriveSpeed = getDriveSpeed();
 
-
             targetAngle = targetSpeedVector.getAngle();
             targetDriveSpeed = targetSpeedVector.getMagnitude();
-            
-            
-            if(Math.abs(targetDriveSpeed) < 0.1){ // was 0.5
+
+            if (Math.abs(targetDriveSpeed) < 0.1) { // was 0.5
                 targetAngle = closest180(currentAngle, targetAngle);
-                if(reversed) targetDriveSpeed *= -1;
-            }else{
-                if(reversed){
+                if (reversed)
+                    targetDriveSpeed *= -1;
+            } else {
+                if (reversed) {
                     targetAngle = closest360(currentAngle, targetAngle + Math.PI);
                     targetDriveSpeed *= -1;
-                }else{
+                } else {
                     targetAngle = closest360(currentAngle, targetAngle);
                 }
             }
@@ -102,63 +107,72 @@ public class SwerveController {
             double targetTurnTick = radToTicks(targetAngle - placement.ang);
             double targetDriveTicksPer100Millis = speedToTicksPer100Millis(targetDriveSpeed);
 
-            if(turn){
+            if (turn) {
                 turnMotor.set(ControlMode.Position, targetTurnTick);
-            }else{
+            } else {
                 turnMotor.set(ControlMode.PercentOutput, 0);
             }
 
-            if(drive){
+            if (drive) {
                 driveMotor.set(ControlMode.Velocity, targetDriveTicksPer100Millis);
-            }else{
+            } else {
                 driveMotor.set(ControlMode.PercentOutput, 0);
             }
+
+            log(name + " target", targetAngle);
+            log(name + " current", currentAngle);
         }
 
-        private double getAngle(){
+        private double getAngle() {
             double encoderPos = turnMotor.getSelectedSensorPosition();
-            return encoderPos / 4096 * (2*Math.PI) + placement.ang;
+            return encoderPos / 4096 * (2 * Math.PI) + placement.ang;
         }
 
-        private double getDriveSpeed(){
+        private double getDriveSpeed() {
             double encoderVel = driveMotor.getSelectedSensorVelocity();
-            return encoderVel / 2048 / DRIVE_RATIO * (2*Math.PI) * WHEEL_RADIUS;
+            return encoderVel / 2048 / DRIVE_RATIO * (2 * Math.PI) * WHEEL_RADIUS;
         }
 
-        private double getDrivePos(){
+        private double getDrivePos() {
             double encoderPos = driveMotor.getSelectedSensorPosition();
-            return encoderPos / 2048 / DRIVE_RATIO * (2*Math.PI) * WHEEL_RADIUS;
+            return encoderPos / 2048 / DRIVE_RATIO * (2 * Math.PI) * WHEEL_RADIUS;
         }
 
-        private double closest360(double currentAngle, double targetAngle){
-            double diffNormalized = Util.normalizeAngle(currentAngle - targetAngle, Math.PI); //angle error from (-PI, PI)
+        private double closest360(double currentAngle, double targetAngle) {
+            double diffNormalized = Util.normalizeAngle(currentAngle - targetAngle, Math.PI); // angle error from (-PI,
+                                                                                              // PI)
             return currentAngle - diffNormalized;
         }
-    
-        private double closest180(double currentAngle, double targetAngle){
-            double differencePi = (currentAngle - targetAngle) % Math.PI; //angle error from (-180, 180)
-    
+
+        private double closest180(double currentAngle, double targetAngle) {
+            double differencePi = (currentAngle - targetAngle) % Math.PI; // angle error from (-180, 180)
+
             double closestAngle;
-            if(Math.abs(differencePi) < (Math.PI / 2.0)){ //chooses closer of the two acceptable angles closest to currentAngle
+            if (Math.abs(differencePi) < (Math.PI / 2.0)) { // chooses closer of the two acceptable angles closest to
+                                                            // currentAngle
                 closestAngle = currentAngle - differencePi;
-            }else{
+            } else {
                 closestAngle = currentAngle - differencePi + Math.copySign(Math.PI, differencePi);
             }
-    
+
             double difference2Pi = (closestAngle - targetAngle) % (2 * Math.PI);
-            reversed = Math.abs(difference2Pi) > (Math.PI / 2.0); //if the difference is closer to 180, reverse direction 
-    
+            reversed = Math.abs(difference2Pi) > (Math.PI / 2.0); // if the difference is closer to 180, reverse
+                                                                  // direction
+
             return closestAngle;
         }
 
-        double radToTicks(double rad){
-            return rad * 4096.0 / (2*Math.PI);
+        double radToTicks(double rad) {
+            return rad * 4096.0 / (2 * Math.PI);
         }
 
-        private double speedToTicksPer100Millis(double speed){
-            return speed / WHEEL_RADIUS / (2*Math.PI) * DRIVE_RATIO * 2048 / 10.0;
+        private double speedToTicksPer100Millis(double speed) {
+            return speed / WHEEL_RADIUS / (2 * Math.PI) * DRIVE_RATIO * 2048 / 10.0;
         }
 
+        private void log(String key, double value) {
+            SmartDashboard.putNumber(key, value);
+        }
     }
 
 }
