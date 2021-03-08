4226c279c9b1f28df70a6be0d7b3521759a8ced4
Neben5 <jlsatpa@gmail.com>
2021-03-07 18:30:01 -0800

    moved alaap's stuff to his own branch

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,7 +4,6 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.SwerveCommand;
-import frc.robot.subsystems.AlaapSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
@@ -23,8 +22,6 @@ public class RobotContainer {
 
     private final SwerveCommand m_swerveCommand = new SwerveCommand(m_swerveSubsystem, () -> stick.getX(),
             () -> stick.getY(), () -> stick.getThrottle());
-
-    AlaapSubsystem a_subsystem = new AlaapSubsystem();
 
     /**
      * The container for the robot. Contains subsystems, OI devices, and commands.
